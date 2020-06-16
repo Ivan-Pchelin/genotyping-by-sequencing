f617f28fb78a60f4366134c3691a780ea03c8559
Ivan-Pchelin <45098892+Ivan-Pchelin@users.noreply.github.com>
2020-06-16 18:08:27 +0300

    Add files via upload

--- attributetogenotypes.py
+++ attributetogenotypes.py
@@ -1,133 +1,132 @@
-# This script compares two fasta files: the dataset "sample.fasta" and file
-# "referenceset.fasta" with reference sequences. The result is the file
-# "attributed.txt", where the first column contains names
-# from "sample.fasta" and the second -- a name from reference dataset, if found
+'''
+This script compares two FASTA files: a dataset "sequence.fasta"
+and the file "referenceset.fasta" with reference sequences.
+The result is the file "attributed.txt", where the names from "sequence.fasta"
+are supplemented with relevant accessions from "referenceset.fasta".
+If there is no perfect match between analyzed sequence and one
+of the reference sequences, no result is indicated.
 
-# Version 2019-01-23
+To launch the script, one should have installed Python interpreter
+from www.python.org/downloads/. Windows users can place the script
+with the files sequence.fasta and referenceset.fasta in the same folder,
+then type cmd in the address bar. In the black window appeared one
+should type "python attributetogenotypes.py", without the quotes.
 
-# Этот блок переписывает изучаемые последовательности в одну линию
-with open('sample.fasta') as inf:
-	with open('neat.fasta', 'w') as ouf:
-		applicant = ''
-		k = 0
-		line = inf.readline()
-		while line:
-			if '>' in line:
-				applicant = applicant.replace('-', '').upper()
-				ouf.write(applicant)
-				if k != 0:
-					ouf.write('\n')
-				k += 1
-				applicant = ''
-				ouf.write(line)
-			else:
-				line = line.replace('\n', '')
-				applicant = applicant + line
-			line = inf.readline()
-		if not line:
-			applicant = applicant.replace('-', '').upper()
-			ouf.write(applicant)
-
-# Этот блок переписывает референсные последовательности в одну линию
-with open('referenceset.fasta') as inf:
-	with open('referenceset_plain.fasta', 'w') as ouf:
-		applicant = ''
-		k = 0
-		line = inf.readline()
-		while line:
-			if '>' in line:
-				applicant = applicant.replace('-','').upper()
-				ouf.write(applicant)
-				if k != 0:
-					ouf.write('\n')
-				k += 1
-				applicant = ''
-				ouf.write(line)
-			else:
-				line = line.replace('\n', '')
-				applicant = applicant + line
-			line = inf.readline()
-		if not line:
-			applicant = applicant.replace('-','').upper()
-			ouf.write(applicant)
-
-# Этот блок ищет референсные последовательности
-# из referenceset_plain.fasta в файле neat.fasta.
-with open('referenceset_plain.fasta') as inf:
-	references = inf.readlines()
+Version 2020-06-16 by Ivan Pchelin, arcella.oraia@gmail.com
+'''
 
 import re
+import os
+import sys
+
+temporaryfiles=set()
+
+# this re-writes FASTA sequences in single lines, capitalizes them
+# and removes spaces
+def irontonew (file):
+    with open(file) as inf:
+        with open('neat_'+file, 'w') as ouf:
+            applicant = ''
+            k = 0
+            line = inf.readline()
+            while line:
+                if '>' in line:
+                    applicant = applicant.replace('-', '').upper()
+                    ouf.write(applicant)
+                    if k != 0:
+                        ouf.write('\n')
+                    k += 1
+                    applicant = ''
+                    ouf.write(line)
+                else:
+                    line = line.replace('\n', '')
+                    applicant = applicant + line
+                line = inf.readline()
+            if not line:
+                applicant = applicant.replace('-', '').upper()
+                ouf.write(applicant)
+    temporaryfiles.add('neat_'+file)
+
+irontonew('referenceset.fasta')
+irontonew('sequence.fasta')
+
+
+analyzednames = []
+analyzednamesSET = set()
+sequences = []
+with open ('neat_sequence.fasta') as inf:
+    lines = inf.readlines()
+    runner = 0
+    while runner < len(lines):
+        c = []
+        analyzednames.append([re.findall(r'\S+', lines[runner])[0]][0])
+        analyzednamesSET.add([re.findall(r'\S+', lines[runner])[0]][0])
+        c.append([re.findall(r'\S+', lines[runner])[0]])
+        c.append([re.findall(r'\S+', lines[runner+1])[0]])
+        sequences.append(c)
+        runner += 2
+
+qwerty = list(analyzednames)
+# check for duplicated sequences
+for i in analyzednamesSET:
+    qwerty.remove(i)
+if len(qwerty) > 0:
+    print('\n', 'Multiple entries of:')
+    for i in qwerty:
+        print(i)
+    print ('\n', '------  Please fix it  ------')
+    for i in temporaryfiles:
+        os.remove(i)
+    sys.exit()
+
+# create dictionaries with reference and analyzed sequences
+references = []
+with open ('neat_referenceset.fasta') as inf:
+    lines = inf.readlines()
+    runner = 0
+    while runner < len(lines):
+        c = []
+        c.append([re.findall(r'\S+', lines[runner])[0]])
+        c.append([re.findall(r'\S+', lines[runner+1])[0]])
+        references.append(c)
+        runner += 2
+
+listwithvariants = []
 generalcount = 0
-poolofnames = set()
-accnos = set()
-with open('neat.fasta') as inf:
-	with open('attributed.txt', 'w') as ouf:
-		line = inf.readline().strip()
-		while line:
-			if line[0] == '>':
-				generalcount += 1
-				elements = re.findall(r'\S+', line)
-				if generalcount > 1:
-					ouf.write('\n')
-				if (elements[0])[1:] in poolofnames:
-					print('Multiple entries of', (elements[0])[1:])
-				ouf.write(elements[0][1:]) # номер изолята
-				poolofnames.add(elements[0][1:])
-			else:
-				runner = 0
-				while runner < len(references):
-					if references[runner].strip() in line:
-						ouf.write('\t')
-						match = re.search(r'^\S+', references[runner-1])#
-						accno = references[runner-1][1:match.end()]
-						ouf.write(accno)
-						accnos.add(accno)
-					runner += 1
-			line = inf.readline().strip()
 
-# Найдем случаи, в которых разные последовательности, приписанные
-# одному и тому же референсу, различаются.
-# Варианты выпишем в variants.fasta
-with open('neat.fasta') as inf:
-	rawseqs = inf.readlines()
+# write the file with genotyping results
+with open ('attributed.txt', 'w') as ouf:
+    for i in references:
+        variantsofcurrentgenotype = set()
+        for j in sequences:
+            if i[1][0] in j[1][0]:
+                generalcount += 1
+                analyzednames.remove(j[0][0])
+                variantsofcurrentgenotype.add(j[1][0])
+                ouf.write (j[0][0][1:])
+                ouf.write ('\t')
+                ouf.write (i[0][0][1:])
+                ouf.write ('\n')
+        if len(variantsofcurrentgenotype) > 1:
+            number = 1
+            for k in variantsofcurrentgenotype:
+                listwithvariants.append(i[0][0]+'-'+'Variant'+str(number))
+                listwithvariants.append(k)
+                number+=1
 
-switch = 0
-with open('attributed.txt') as inf:
-	with open('variants.fasta', 'w') as ouf:
-		line = inf.readline().strip()
-		while line:
-			accnu = re.findall(r'\S+', line)
-			accno = accnu[-1]
-			if accno in accnos:
-				caption = '>' + accno + '-'
-				accnos.remove(accno)
-				runner = 0
-				while runner < len(references):
-					if accno in references[runner]:
-						currentseq = references[runner+1].strip()
-					runner += 1
-				runner = 0
-				currentvariants = set()
-				while runner < len(rawseqs):
-					if currentseq in rawseqs[runner]:
-						currentvariants.add(rawseqs[runner])
-					runner += 1
-				runner = 1
-				if len(currentvariants) > 1:
-					switch = 1
-					for i in currentvariants:
-						ouf.write(caption)
-						ouf.write(str(runner))
-						ouf.write('\n')
-						ouf.write(i)
-						runner += 1
-			line = inf.readline()
+# write the file with sequence variants, if any
+if listwithvariants != []:
+    with open('variants.fasta', 'w') as ouf:
+        for i in listwithvariants:
+            ouf.write(i)
+            ouf.write('\n')
 
 print('\n', generalcount, 'sequences analyzed')
+if len(analyzednames) > 0:
+    print('\n', 'No matches for:')
+    for i in analyzednames:
+        print ('', i[1:])
 
-# Удалим временные файлы
-import os
-os.remove('referenceset_plain.fasta')
-os.remove('neat.fasta')
-if switch == 0:
-	os.remove('variants.fasta')
+# delete temporary files
+for i in temporaryfiles:
+    os.remove(i)
